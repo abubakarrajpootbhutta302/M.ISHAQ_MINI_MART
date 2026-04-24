838a1466396c161fed2848514343102832972cf8
abubakarrajpootbhutta302 <abubakar10082009@gmail.com>
2026-04-24 04:31:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let buttons = document.querySelectorAll("button");
+let cart = [];
 
-  buttons.forEach(btn => {
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.querySelectorAll(".card button");
+
+  buttons.forEach((btn, index) => {
     btn.addEventListener("click", () => {
-      alert("Item added to cart 🛒");
+      
+      let product = btn.parentElement.querySelector("h3").innerText;
+
+      cart.push(product);
+
+      alert(product + " added to cart 🛒");
+
+      console.log("Cart:", cart);
     });
   });
 });
