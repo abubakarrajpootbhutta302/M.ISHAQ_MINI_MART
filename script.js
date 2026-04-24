049a068be285c1b90f9092afb0306092637c21e5
abubakarrajpootbhutta302 <abubakar10082009@gmail.com>
2026-04-24 04:27:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-let buttons = document.querySelectorAll("button");
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.querySelectorAll("button");
 
-buttons.forEach(btn => {
-  btn.addEventListener("click", () => {
-    alert("Item added to cart 🛒");
+  buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+      alert("Item added to cart 🛒");
+    });
   });
 });
